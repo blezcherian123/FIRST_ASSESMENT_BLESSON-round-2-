f80836ba0fb03c288a87b6d9b81fd634d7a750de
blezcherian <blezcherian07@gmail.com>
2025-04-18 14:07:10 +0530

    Updated index.html, added logo, and modified logs

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Blez cherian\Desktop\dbs_bank\venv
+set VIRTUAL_ENV=C:\Users\Blez cherian\Desktop\dbs_bank\FIRST_ASSESMENT_BLESSON-round-2-\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
